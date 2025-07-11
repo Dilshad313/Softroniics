0488d49995be890193f98633eb98694a71741c4f
Dilshad <diluk2005786@gmail.com>
2025-07-11 11:18:32 +0530

    E-commerce completed

---
+++ api/fetch.js
@@ -0,0 +1,26 @@
+async function fetchdata(){
+    try {
+        const res = await fetch("https://fakestoreapi.com/products");
+        console.log(res);
+        const products = await res.json();
+        console.log(products);
+        str = ``
+        products.map((pro) => {
+            str += `
+            <div class="card">
+                <img src="${pro.image}" alt="${pro.title} width="170px height="170px">
+                <h2 style="color: red;">${pro.title}</h2>
+                <h3 style="color:gray;">Price: $${pro.price}</h3>
+                <p>Category: ${pro.category}</p>
+                <p>${pro.description}</p>
+            </div>
+            `;
+            document.getElementById("container").innerHTML = str;
+        })
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+fetchdata()
